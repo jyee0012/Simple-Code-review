758d409944b11a354ffb682314f8b7fde8499677
jyee0012 <jyee12@studentmail.nait.ca>
2018-01-09 09:46:49 -0700

    Created Visual Studio Console App

---
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReview
+{
+    public class Salutation
+    {
+        private string hello, goodbye;
+        public Salutation(string greeting, string farewell)
+        {
+            hello = greeting;
+            goodbye = farewell;
+        }
+        public string Greet()
+        {
+            return hello;
+        }
+        public string Farewell()
+        {
+            return goodbye;
+        }
+    }
+}
