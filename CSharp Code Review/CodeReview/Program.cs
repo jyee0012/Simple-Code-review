928bc47fdee9c7a8519add691e9bece0775a4074
jyee0012 <jyee12@studentmail.nait.ca>
2018-01-09 09:50:33 -0700

    Created Walmart Greeter

--- CSharp Code Review/CodeReview/Program.cs
+++ CSharp Code Review/CodeReview/Program.cs
@@ -11,7 +11,11 @@ namespace CodeReview
         Salutation salutate = new Salutation("hello","goodbyey");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello World");
+            // salutation for a walmart greeter.
+            Salutation oldGuy = new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!");
+            Console.WriteLine(oldGuy.Greet());
+            Console.WriteLine(oldGuy.Farewell());
 
             Console.ReadKey();
         }
