890b1a5951f294b60a37a643ec47a7bf2f16baaa
jyee0012 <jyee12@studentmail.nait.ca>
2018-01-11 16:23:34 -0700

    Created Program

--- CSharp Code Review/CodeReview/Program.cs
+++ CSharp Code Review/CodeReview/Program.cs
@@ -8,16 +8,69 @@ namespace CodeReview
 {
     class Program
     {
-        Salutation salutate = new Salutation("hello","goodbyey");
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World");
-            // salutation for a walmart greeter.
-            Salutation oldGuy = new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!");
-            Console.WriteLine(oldGuy.Greet());
-            Console.WriteLine(oldGuy.Farewell());
-
+            var app = new Program();
+            app.Run();
+            
+            Console.Write("Press any key to continue.....");
             Console.ReadKey();
+        }
+
+        // Our "Program" is really the "heart" of our "application"
+        // So, Let's treat it like an object.
+        // We'll use a property that holds a bunch of Salutation objects.
+        
+        public List<Salutation> Speakers { get; private set; }
+        public Program()
+        {
+            // The job of a constructor is to make sure that all the properties/fields of the object are set to "meaningful values"
+            Speakers = new List<Salutation>();
+            Speakers.Add(new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!","oldGuy"));
+            Speakers.Add(new Salutation("nuqneH!", "Qapla'!", "klingonCommander"));
+        }
+        void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("You have entered a large building. {0} beings stand before you.", Speakers.Count);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Who do you want to speak to? ");
+            foreach (Salutation person in Speakers)
+            {
+                Console.Write(person.name);
+                if (person != Speakers[Speakers.Count-1])
+                {
+                    Console.Write(" or ");
+                }
+                else
+                {
+                    Console.Write("?");
+                }
+            }
+            string input = "C";
+            while (input != "A" && input != "B")
+            {
+                input = Console.ReadLine().ToUpper();
+            }
+            Console.ResetColor();
+            switch(input)
+            {
+                case "A":
+                    GreetAndFarewell(Speakers[0]);
+                    break;
+                case "B":
+                    GreetAndFarewell(Speakers[1]);
+                    break;
+                default:
+                    break;
+            }
+        }
+        void GreetAndFarewell(Salutation person)
+        {
+            Console.WriteLine(person.Greet());
+            Console.WriteLine(person.Farewell());
+
+            Console.WriteLine();
         }
     }
 }
--- CSharp Code Review/CodeReview/Salutation.cs
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 namespace CodeReview
 {
+    /// <summary>
+    /// The Salutation class represents something that can say hello or goodbye
+    /// </summary>
     public class Salutation
     {
+        #region Variables
+        // a computer program is a set of instruction for manipulating information
         private string hello, goodbye;
-        public Salutation(string greeting, string farewell)
+        public string name { get; set; }
+        #endregion
+     
+        #region Constructor
+        /// <summary>
+        /// Instantiates a Salutation object with appropriate messages
+        /// </summary>
+        /// <param name="greeting">The message to use when greeting</param>
+        /// <param name="farewell">The message to use when saying farewell</param>
+        public Salutation(string greeting, string farewell, string person)
         {
             hello = greeting;
             goodbye = farewell;
+            name = person;
         }
         public string Greet()
         {
@@ -22,5 +37,6 @@ namespace CodeReview
         {
             return goodbye;
         }
+        #endregion
     }
 }
